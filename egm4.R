42ea309076c94a48990c1028b7f4b3cc20d816a0
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-04-17 12:24:12 -0400

    Improved comments, flux calculation, plotting

--- egm4.R
+++ egm4.R
@@ -2,15 +2,27 @@
 # R script (written using 3.0.3) to process EGM-4 data
 # BBL April 2014
 
-# IMPORTANT: see the NOTEs below
+# To use:
+# 	1. Set SYSTEM_VOLUME and CHAMBER_AREA variables below
+#	2. Set MEAS_INTERVAL variable
+#	3. If using plot-specific ancillary data, set PLOTDATA variable
+#			and make sure file is in correct format
+#	4. Set INPUT_DIR to location of EGM4 output files
+#	5. Check the flux calculations in compute_flux() for your use case
+#	6. source() this file.
+#			start with a simple test case, and carefully check log file and outputs!
 
 # Important variable definitions, esp. data source & destination
-
 SCRIPTNAME		<- "egm4.R"
 INPUT_DIR		<- "sampledata/"
-PLOTDATA		<- "sampledata/plotdata.csv"
 OUTPUT_DIR		<- "outputs/"
 LOG_DIR			<- "logs/"
+
+# The optional PLOTDATA file *must* have a 'Plot' field
+# It *may* have 'Mass' and/or 'Area' fields, which will be divided into the computed flux
+#	- if present, the 'Area' field will override CHAMBER_AREA below
+# It *may* have a 'Volume' field, cm3, which will be added to SYSTEM_VOLUME for each plot
+PLOTDATA		<- "sampledata/plotdata.csv"
 
 SYSTEM_VOLUME	<- 15.15 / 100^3		# m^3
 CHAMBER_AREA	<- 5.5					# cm^2
@@ -18,9 +30,9 @@ CHAMBER_AREA	<- 5.5					# cm^2
 SEPARATOR		<- "-------------------"
 
 # This is a critical variable, since seconds don't appear in the EGM4 file
+# Time between successive measurements (s)
 MEAS_INTERVAL	<- 10
 
-# Support functions and common definitions
 
 # -----------------------------------------------------------------------------
 # Time-stamped output function
@@ -53,7 +65,7 @@ savedata <- function( df, extension=".csv" ) {
 	fn <- paste0( OUTPUT_DIR, "/", deparse( substitute( df ) ), extension )
 	printlog( "Saving", fn )
 	write.csv( df, fn, row.names=F )
-} # saveplot
+} # savedata
 
 # -----------------------------------------------------------------------------
 # Open a csv file and return data
@@ -86,11 +98,16 @@ read_egmfile <- function( fn ) {
 	stopifnot( file.exists( fqfn ) )
 	d <- read.table( fqfn, comment.char=";", sep="\t" )
 	printdims( d )
+	# TODO: check that the file terminates with a "Logging stopped after X records" line
 	
-	# TODO: get these columns directly from file?
-	names( d ) <- c( "Plot", "RecNo", "Day", "Month", "Hour", "Min", "CO2_Ref", "mb_Ref",
+	# TODO: get these columns directly from file
+	egm4names <- c( "Plot", "RecNo", "Day", "Month", "Hour", "Min", "CO2_Ref", "mb_Ref",
 		 "mbR_Temp", "Input_A", "Input_B", "Input_C", "Input_D", "Input_E", "Input_F", 
 		 "Input_G", "Input_H", "ATMP", "Probe Type" )
+	stopifnot( length( names( d ) )==length( egm4names ) )
+	names( d ) <- egm4names
+	
+	# Add ancillary data
 	d$filename <- fn
 	
 	# Seconds don't appear in the EGM-4 output, which is weird and sucks. We fill them in
@@ -128,14 +145,14 @@ read_plotdata <- function( fn=PLOTDATA ) {
 	}
 	
 	return( d )
-}
+} # read_plotdata
 
 # -----------------------------------------------------------------------------
 # compute fluxes
 compute_flux <- function( d ) {
 
 	m <- lm( CO2_Ref ~ Sec, data=d )
-	Resp_raw <- as.numeric( coef( m )[ 2 ] )	# i.e. the slope
+	resp_raw <- as.numeric( coef( m )[ 2 ] )	# i.e. the slope
 	
 	# We want to convert raw respiration (d[CO2]/dt) to a flux using
 	# A = dC/dt * V/S * Pa/RT (e.g. Steduto et al. 2002), where
@@ -151,15 +168,15 @@ compute_flux <- function( d ) {
 
 	S 			<- CHAMBER_AREA		# note cm2, not m2!
 	if( any( names( d )=="Area" ) ) {
-		 S <- d$Area
+		 S <- mean( d$Area )
 	}
 	V 			<- SYSTEM_VOLUME
 	if( any( names( d )=="Volume" ) ) {
-		V <- V + d$Volume
+		V <- V + mean( d$Volume )
 	}
 	M 			<- 1.0
 	if( any( names( d )=="Mass" ) ) {
-		 M <- d$Mass
+		 M <- mean( d$Mass )
 	}
 	
 	Pa 			<- 101						# kPa
@@ -168,12 +185,12 @@ compute_flux <- function( d ) {
 	Tair <- mean( d$Input_C )		# assumes EGM temperature probe connected
 	
 	# Calculate mass- (or area-) corrected respiration, umol/g soil/s or umol/cm2/s
-	Resp_mass <- Resp_raw * V/S/M * Pa/( R*( 273.1+Tair ) )
+	resp_corrected <- resp_raw * V/S/M * Pa/( R*( 273.1+Tair ) )
 
 	# Convert from umol/g soil/s to mgC/kg soil/day or whatever
 	# NOTE: you probably want to change this line for your specific setup
-	flux <- Resp_mass / 1e6 * 12 * 1000 * 1000 * 60 * 60 * 24
-
+	flux <- resp_corrected / 1e6 * 12 * 1000 * 1000 * 60 * 60 * 24
+printlog(nrow(d),length(flux))
 	return( c( Tair=Tair, V=V, S=S, Day=mean( d$Day ), Month=mean( d$Month ), N=nrow( d ), flux=flux ) )
 }
 
@@ -208,15 +225,17 @@ printlog( "All done reading data." )
 printdims( alldata )
 
 plotdata <- read_plotdata()
-printlog( "Merging respiration data with dry mass data..." )
-alldata <- merge( plotdata, alldata )
+if( !is.null( plotdata ) ) {
+	printlog( "Merging respiration data with dry mass data..." )
+	alldata <- merge( plotdata, alldata )
+}
 
 printlog( "Computing fluxes..." )
 fluxes <- ddply( alldata, .( filename, Plot ), .fun=compute_flux )
 
 print( summary( fluxes ) )
 
-p <- ggplot( fluxes, aes( Plot, flux ) ) + geom_point()
+p <- ggplot( fluxes, aes( Day, flux, group=Plot, colour=Plot ) ) + geom_point() + geom_line()
 print( p )
 saveplot( "flux_summary" )
 
